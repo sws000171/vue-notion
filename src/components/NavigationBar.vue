11b7988413895f19b7ac01cf79e8f788c40cdb13
sws000171 <higuchi@software-s.co.jp>
2024-01-26 00:53:20 +0900

    test

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,43 @@
+<template>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="main">TechpitNotion</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav me-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="/main"><i class="fas fa-yin-yang"></i>ノートリスト</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+          <i class="fas fa-pencil-alt"></i>学習用ページ
+        </a>
+        <ul class="dropdown-menu">
+          <li><a class="dropdown-item" href="/study/binding">学習１</a></li>
+          <li><a class="dropdown-item" href="/study/directive">学習２</a></li>
+          <li><a class="dropdown-item" href="/study/event">学習３</a></li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</nav>
+</template>
+  
+  <script>
+  export default {
+  }
+  </script>
+  
+  <style scoped>
+  .navigation-bar {
+    height: 60px;
+    /*
+    color: white;
+    background-color: #6c757d;
+    */
+  }
+  </style>
+  
---
+++ src/components/study/EventPage.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="event-page">
+        <h2>学習３：イベント</h2>
+        <!--従来のJavaScript-->
+        <h3><span class=badge badge-secondary>JavaScript</span></h3>
+        <span id="countText">クリックした回数は0回です</span>
+        <button id ="button" class="btn btn-sm btn-info">Click Me</button>
+        <br/>
+        <br/>
+        <!--Vue.js-->
+        <h3><span class=badge badge-secondary>Vue.js</span></h3>
+        <span>クリックした回数は{{ count }}回です</span>
+        <button id = "button" class="btn btn-sm btn-info" @click="onClickButton()">Click Me</button>
+    </div>
+</template>
+
+<script>
+//従来のJavaScript
+let count = 0;
+window.onload = ()=> {
+    const button = document.getElementById('button');
+    button.onclick = ()=> {
+        count ++;
+        //オブジェクトの変更はできないがプロパティの変更は可能なのでconst
+        const countText = document.getElementById('countText');
+        countText.innerHTML = 'クリックした回数は' + count + '回です'; 
+    };
+}
+export default{
+    data(){
+        return{
+            count : 0
+        }
+    },
+    methods : {
+        onClickButton() {
+            this.count++;
+        }
+    }
+}
+</script>
+
+<style scoped>
+.event-page{
+    padding: 20px;
+}
+</style>
